0210c7056c5eb65ccd7b133ea6a8a197e8ad18ef
Felipe-Sabino-d-Oliveira <fsabino006@gmail.com>
2024-04-24 15:11:23 -0300

    novas mudanças!!! footer feio!

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,110 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+    <v-main class="body">
+      <div>
+        <!-- ------ BARRA DE FERRAMENTAS ------ -->
+        <v-toolbar
+          image="https://ficr.catolica.edu.br/portal/wp-content/uploads/2019/07/Fachada-FICR.jpg"
+          dark
+          prominent
+        >
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Tela de Cursos</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-export</v-icon>
+          </v-btn>
+        </v-toolbar>
       </div>
 
-      <v-spacer></v-spacer>
+      <!-- ------------------- BODY ------------------- -->
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+      <div class="container">
+        <v-container class="adicionar__curso">
+          Inserir cursos
+        </v-container>
 
-    <v-main>
-      <HelloWorld/>
+        <v-container class="adicionar__curso">
+          Inserir cursos
+        </v-container>
+
+        <v-container class="adicionar__curso">
+          Inserir cursos
+        </v-container>
+      </div>
+
     </v-main>
+
+    <!-- FOOTER -->
+
+    <v-footer class="d-flex flex-column">
+      <div class="bg-teal d-flex w-100 align-center px-4">
+        <strong>Get connected with us on social networks!</strong>
+
+        <v-spacer></v-spacer>
+
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          :icon="icon"
+          class="mx-4"
+          size="small"
+          variant="plain"
+        ></v-btn>
+      </div>
+
+      <div class="px-4 py-2 bg-black text-center w-100">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </div>
+    </v-footer>
+
   </v-app>
 </template>
 
+
+<!-- ----------------------- SCRIPT ----------------------- -->
+
 <script>
-import HelloWorld from './components/HelloWorld';
+
+
+
+
+/* ----- FOOTER ----- */
 
 export default {
-  name: 'App',
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
 
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
 </script>
+
+<style>
+
+.body{
+  font-size: 62.5%;
+}
+
+.container{
+  align-items: center;
+  
+  max-width: 1152px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+}
+
+.adicionar__curso{
+  font-size: 5.0em;
+}
+
+</style>
